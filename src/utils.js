cbd094fe2f756ad85cf1f476e35e4d0d1218b6e8
tonymk0987 <k.tony.muriuki@gmail.com>
2024-05-12 20:51:29 +0300

    Done

--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,8 @@
 // Your code here
+export function pointsForWord(word) {
+    let points = 0;
+    for (const char of word) {
+      points += /[aeiou]/.test(char) ? 1 : 2;
+    }
+    return points;
+  }
